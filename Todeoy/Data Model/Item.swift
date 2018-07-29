5551d5e4d72faa7c507536575a580643b5295cf0
Josh Bourke <joshbourke@joshs-mbp>
2018-07-29 18:03:40 +1000

    convert to MVC design pattern.

---
+++ Todeoy/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todeoy
+//
+//  Created by Josh Bourke on 29/7/18.
+//  Copyright © 2018 Josh Bourke. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
